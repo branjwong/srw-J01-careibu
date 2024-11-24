a64521a6085e74ff45b6ac31cfc84a66b9536f7c
Brandon Wong <branjwong@hey.com>
2024-11-24 23:28:32 +0000

    format documents

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,8 +4,8 @@ import { USER_TOKEN } from "@/lib/auth";
 import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
-    const cookieStore = await cookies();    
+    const cookieStore = await cookies();
     cookieStore.delete(USER_TOKEN);
-   
+
     redirect('/');
 }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,16 +3,16 @@ import { test, expect } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
     await page.goto('./');
 });
-  
+
 test('has title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Careibu/);
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Careibu/);
 });
 
 test('clicking login link goes to login page', async ({ page }) => {
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Log in' }).click();
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Log in' }).click();
 
-  // Expects URL to redirect to login.
-  await expect(page).toHaveURL(/.*login/);
+    // Expects URL to redirect to login.
+    await expect(page).toHaveURL(/.*login/);
 });
