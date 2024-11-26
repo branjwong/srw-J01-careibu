07d99c9f318d7c00d0d542193d0250f3685042c6
Brandon Wong <branjwong@hey.com>
2024-11-26 09:51:13 +0000

    clean home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
                         className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     />
                 </div>
-                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                <div className="mx-auto max-w-2xl py-16 sm:py-24">
                     <div className="text-center">
                         <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
                             You are young and want to mean something to someone
@@ -63,12 +63,6 @@ export default function Home() {
                                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Make an Impact as a Volunteer
-                            </a>
-                            <a
-                                href="#"
-                                className="text-sm/6 font-semibold text-gray-900"
-                            >
-                                Learn more <span aria-hidden="true">→</span>
                             </a>
                         </div>
                     </div>
