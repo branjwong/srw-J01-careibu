802937991d0a604bc9dade5332f9b9a279075d6c
Brandon Wong <branjwong@hey.com>
2024-11-26 08:42:30 +0000

    fix sidebar

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -4,15 +4,17 @@ export default function Dashboard() {
     return (
         <Navigation>
             <main className="md:col-span-9 p-4">
-                <header className="bg-white shadow">
+                <div className="bg-white shadow">
+                    <header>
+                        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                                Dashboard
+                            </h1>
+                        </div>
+                    </header>
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                            Dashboard
-                        </h1>
+                        {/* Your content */}
                     </div>
-                </header>
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    {/* Your content */}
                 </div>
             </main>
         </Navigation>
--- src/app/dashboard/onboarding/background/page.tsx
+++ src/app/dashboard/onboarding/background/page.tsx
@@ -1,18 +1,23 @@
 import Navigation from "../../ui/navigation";
+import Sidebar from "../ui/sidebar";
 
 export default function Background() {
     return (
         <Navigation>
-            <header className="bg-white shadow">
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                        Await Background Check
-                    </h1>
+            <Sidebar>
+                <div className="bg-white shadow">
+                    <header>
+                        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                                Await Background Check
+                            </h1>
+                        </div>
+                    </header>
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        {/* Your content */}
+                    </div>
                 </div>
-            </header>
-            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                {/* Your content */}
-            </div>
+            </Sidebar>
         </Navigation>
     );
 }
