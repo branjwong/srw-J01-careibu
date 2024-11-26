6460a89ac912c514df74edf9d9eaa8dd35d24862
Brandon Wong <branjwong@hey.com>
2024-11-26 07:59:00 +0000

    add dashboard hierarchy

---
+++ src/app/dashboard/onboarding/personal/page.tsx
@@ -0,0 +1,16 @@
+export default function Personal() {
+    return (
+        <span>
+            <header className="bg-white shadow">
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                        Submit Personal Information
+                    </h1>
+                </div>
+            </header>
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {/* Your content */}
+            </div>
+        </span>
+    );
+}
